5160c19d67ce3ccf7b18fa8b43b111e1fff4bba5
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2017-05-19 18:33:31 +0300

    Bug fix

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from text_classifier.code.classifier import Classifier
-from text_classifier.code.text_handler import TextHandler
+from code.classifier import Classifier
+from code.text_handler import TextHandler
 
+# Text operation handler
 handler = TextHandler()
 
+# Text classifier (implemented with using Naive Bayes algorithm)
 classifier = Classifier(handler.get_train_data(), handler.get_vocabulary())
 
 
-f = open('output.txt', 'w')
-
 index = 1
 test_instance_count = 5
+f = open('output.txt', 'w')
 for test_id, test_instance in handler.get_test_data():
     # Find the class
     probability, category = classifier.find_class(test_instance)
